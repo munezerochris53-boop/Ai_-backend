62de454f31ed9fc4d7047e38329d918be28fa6b0
munezerochris53-boop <munezerochris53@gmail.com>
2026-02-01 14:59:40 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // 🔑 API KEYS (ushobora kongeramo 30+)
 $API_KEYS = [
-    "sk-key-1",
-    "sk-key-2",
-    "sk-key-3",
+    "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",
+    "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",
+    "AIzaSyCdXx3OdVMPm9_8d2L8X2HBv4a5Slhkekc","AIzaSyDN3l8Ehh2w4kSXZXGncBOw1qtJcy2zR1k"
     // ...
     // kugeza kuri 30+
 ];
